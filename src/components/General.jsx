ce3e7c8a1ac3f4eeea91cd825cd66df67713a6e1
Giao Vu Dinh <49886137+tvu20@users.noreply.github.com>
2022-06-27 16:58:20 -0400

    skeleton

---
+++ src/components/General.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import generalData from "../data/general-info";
+
+export const General = () => {
+  return (
+    <>
+      <div>{generalData.title}</div>
+    </>
+  );
+};

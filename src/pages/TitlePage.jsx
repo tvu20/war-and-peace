a8c44a35c546b8a72a18b9c529f5b99f1cd02daa
Giao Vu Dinh <49886137+tvu20@users.noreply.github.com>
2022-07-01 18:50:22 -0400

    title fonts

--- src/pages/TitlePage.jsx
+++ src/pages/TitlePage.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 
+import "../styles/title.css";
+
 import { ScrollingBackground } from "../components/ScrollingBackground";
 
 export const TitlePage = (props) => {
   return (
-    <div>
-      <ScrollingBackground />
-      {/* <h1>War and Peace</h1> */}
+    <div className="page-container fade-in">
+      {/* <ScrollingBackground /> */}
+      <div className="title-container">
+        <h1>War and Peace</h1>
+        <h2>Original Novel by Leo Tolstoy</h2>
+        <p>
+          A brief textual analysis of one of the most highly praised pieces of
+          world classical literature, presented as an interactive data
+          visualization essay.
+        </p>
+        <h4>Created by Giao Vu Dinh | Originally published in July 2022</h4>
+      </div>
     </div>
   );
 };
